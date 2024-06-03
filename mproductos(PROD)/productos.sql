f5ac7acebbee4371072b4c561cc28f576dcd1261
Itzel-NTC <nayeliticona@gmail.com>
2024-06-03 12:38:46 -0400

    base de datos módulo de productos

--- mproductos(PROD)/productos.sql
+++ mproductos(PROD)/productos.sql
@@ -12,33 +12,19 @@ SET time_zone = "+00:00";
 
 START TRANSACTION;
 
--- Configuración de charset y collation
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
--- Base de datos: `users_crud_php`
-
--- Estructura de tabla para la tabla `productos`
-CREATE TABLE `productos` (
-  `id_producto` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(255) NOT NULL,
-  `descripcion` text DEFAULT NULL,
-  `precio` decimal(10,2) NOT NULL,
-  `categoria` varchar(255) NOT NULL,
-  `estado` enum('activo','inactivo') NOT NULL DEFAULT 'activo',
-  `imagen_principal` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_producto`)
+CREATE TABLE productos (
+  id_producto INT NOT NULL AUTO_INCREMENT,
+  nombre VARCHAR(255) NOT NULL,
+  descripcion TEXT DEFAULT NULL,
+  precio DECIMAL(10,2) NOT NULL,
+  categoria VARCHAR(255) NOT NULL,
+  estado ENUM('activo','inactivo') NOT NULL DEFAULT 'activo',
+  imagen_principal VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (id_producto)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Volcado de datos para la tabla `productos`
-INSERT INTO `productos` (`nombre`, `descripcion`, `precio`, `categoria`, `estado`, `imagen_principal`) VALUES
+INSERT INTO productos (nombre, descripcion, precio, categoria, estado, imagen_principal) VALUES
 ('Sal', 'Sal yodada', 35.00, 'sal', 'activo', '');
 
 COMMIT;
-
--- Restaurar configuración de charset y collation
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
