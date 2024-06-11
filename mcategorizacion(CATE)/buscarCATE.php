b847f8253b166ed44db6071bcb4fea604dc68f9c
PequeLes <les062001ori@gmail.com>
2024-06-11 16:42:32 -0400

    parteLeslie

---
+++ mcategorizacion(CATE)/buscarCATE.php
@@ -0,0 +1,70 @@
+=
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Evaluación</title>
+    <link rel="stylesheet" href="../mcategorizacion(CATE)/assets/styles/styleCATE.css">
+    <style>
+        /* Estilos específicos para dispositivos móviles */
+        @media (max-width: 600px) {
+            body {
+                font-size: 14px;
+                /* Reducimos el tamaño de fuente para dispositivos móviles */
+            }
+
+            table {
+                width: 100%;
+                border-collapse: collapse;
+            }
+
+            th,
+            td {
+                border: 1px solid black;
+                padding: 8px;
+                text-align: left;
+            }
+
+            tr:nth-child(even) {
+                background-color: #f2f2f2;
+            }
+
+            th,
+            td {
+                display: block;
+                width: 100%;
+                box-sizing: border-box;
+                padding: 10px;
+                text-align: left;
+            }
+
+            td {
+                border: none;
+                border-bottom: 1px solid #ddd;
+            }
+
+            input[type="number"] {
+                width: calc(100% - 20px);
+                /* Restamos el padding de la tabla */
+                padding: 5px;
+                font-size: 1em;
+                border: 1px solid #ccc;
+                border-radius: 4px;
+                text-align: center;
+                background-color: #f9f9f9;
+                transition: background-color 0.3s ease, border-color 0.3s ease;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <div class="App">
+        <h1 class="title-compras">EVALUACIÓN</h1>
+        <h1 class="title-compras">DE DESEMPEÑO</h1>
+    </div>
+</body>
+
+</html>
